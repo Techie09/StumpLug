ee1d4a17bcd181bf222d6a5447880802a417df43
feathea1 <feathea1@ATC-176-08>
2016-09-29 14:25:31 -0400

    F2; Implemented Student Object

--- stumpLugApp/stumpLugApp/Bus/Student.cs
+++ stumpLugApp/stumpLugApp/Bus/Student.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace stumpLugApp.Bus
 {
-    class Student
+    public class Student
     {
+        private string m_id;
+        private string m_firstName;
+        private string m_lastName;
+        private List<EnrolledCourse> m_courses;
+
+        public string Id
+        {
+            get
+            {
+                return m_id;
+            }
+            set
+            {
+                m_id = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return m_firstName;
+            }
+            set
+            {
+                m_firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return m_lastName;
+            }
+            set
+            {
+                m_lastName = value;
+            }
+        }
+        public List<EnrolledCourse> Courses
+        {
+            get
+            {
+                return m_courses;
+            }
+            set
+            {
+                m_courses = value;
+            }
+        }
     }
 }
