332d59bf16c09b5503a834216802451f8155d4fd
Vince A <andreav@ferris.edu>
2016-10-03 18:10:36 -0400

    Implemented navigation skeleton
    Currently an issue exists with the student search command (^s), will investigate further, or we could just change the command to something that doesn't use Ctrl.

---
+++ stumpLugApp/stumpLugApp/UI/InputArgs.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stumpLugApp.UI
+{
+    public class InputArgs
+    {
+        private ConsoleKeyInfo m_keyInfo = new ConsoleKeyInfo();
+        public ConsoleKeyInfo KeyInfo
+        {
+            get
+            {
+                return m_keyInfo;
+            }
+            set
+            {
+                m_keyInfo = value;
+            }
+        }
+
+        public ConsoleKey Key
+        {
+            get { return KeyInfo.Key; }
+        }
+
+        public bool isShiftKeyPressed
+        {
+            get { return (KeyInfo.Modifiers & ConsoleModifiers.Shift) != 0; }
+        }
+
+        public bool isAltKeyPressed
+        {
+            get { return (KeyInfo.Modifiers & ConsoleModifiers.Alt) != 0; }
+        }
+
+        public bool isCtrlKeyPressed
+        {
+            get { return (KeyInfo.Modifiers & ConsoleModifiers.Control) != 0; }
+        }
+        /*
+        private ConsoleKey m_actionKey; // char which determines the action [ Add, Create, Exit : A,C,X ]
+        private ConsoleKey m_typeKey; // char which determines the type [ Student, Course : S,C ]
+
+        public ConsoleKey ActionKey
+        {
+            get
+            {
+               return m_actionKey;
+            }
+            set
+            {
+                m_actionKey = value;
+            }
+        }
+        public ConsoleKey TypeKey
+        {
+            get
+            {
+                return m_typeKey;
+            }
+            set
+            {
+                m_typeKey = value;
+            }
+         
+        }
+        */
+
+    }
+}
---
+++ stumpLugApp/stumpLugApp/UI/MainMenuPage.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stumpLugApp.UI
+{
+    public class MainMenuPage : Page
+    {
+        private string header = "Main Menu";
+
+        public override void OnLoad()
+        {
+            base.OnLoad();
+            Console.WriteLine(header);
+            Console.WriteLine(horzRule);
+            Console.WriteLine("Alt + X | Exit");
+            Console.WriteLine("Shift + S | Search for a Student");
+            Console.WriteLine("Ctrl + S | Add a Student");
+            Console.WriteLine("Shift + C | Search for a Course");
+            Console.WriteLine("Ctrl + C | Add a Course");
+
+
+            HandleInput();
+                      
+        }
+
+        public override void NavigateTo(Page page)
+        { 
+            base.NavigateTo(page);
+            if (page.GetType() == typeof(AddStudentPage)) 
+            {
+                PageManager.Load(page);
+            }
+            else if (page.GetType() == typeof(SearchStudentPage))
+            {
+                PageManager.Load(page);
+            }
+            else if (page.GetType() == typeof(AddCoursePage))
+            {
+                PageManager.Load(page);
+            }
+            else if (page.GetType() == typeof(SearchCoursePage))
+            {
+                PageManager.Load(page);
+            }
+        }
+
+        public override void HandleInput()
+        {
+            InputArgs input = GetInput();
+            if (input.isAltKeyPressed && input.Key == ConsoleKey.X)
+            {
+                Console.Clear();
+                if (ExitPrompt())
+                {
+                    Environment.Exit(0);
+                }
+                else NavigateTo(new MainMenuPage());
+            }
+            if (input.isShiftKeyPressed && input.Key == ConsoleKey.S)
+            {
+                NavigateTo(new SearchStudentPage());
+            }
+            if (input.isCtrlKeyPressed && input.Key == ConsoleKey.S)
+            {
+                NavigateTo(new AddStudentPage());
+            }
+            if (input.isShiftKeyPressed && input.Key == ConsoleKey.C)
+            {
+                NavigateTo(new SearchCoursePage());
+            }
+            if (input.isCtrlKeyPressed && input.Key == ConsoleKey.C)
+            {
+                NavigateTo(new AddCoursePage());
+            }
+            else
+                HandleInput();
+        }
+    }
+}
